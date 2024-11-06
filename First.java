6c52b88dc1c0c9fc820e90503aa2aaa7cdfcfc6f
Keerthi-chowdary595 <keerthichowdarykolli8@gmail.com>
2024-11-06 16:36:19 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,16 @@
+//Sample program
+/*sample
+    java
+     program
+*/
+import java.lang.System;
+import java.lang.String;
+//import java.lang.*;
+
+class First
+{
+public static void main(String args[])
+{
+System.out.println("Welcome to the java First Program");
+}
+}
